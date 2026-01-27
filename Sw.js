4bf58326981ff44fabcc565b84e6aec99081ca3a
Ahmed Ali <arain114u@gmail.com>
2026-01-27 11:50:22 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,18 @@
+const cacheName = "puzzle-pro-v1";
+const files = [
+ "./",
+ "./index.html",
+ "./manifest.json"
+];
+
+self.addEventListener("install", e=>{
+ e.waitUntil(
+  caches.open(cacheName).then(cache=>cache.addAll(files))
+ );
+});
+
+self.addEventListener("fetch", e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
